f32345ddee13323ef9ec49199a7892b84f060e8c
Pedro <pedro.trabalhos@outlook.com>
2022-06-17 22:35:34 -0300

    created automation to insert updated data in mysql

---
+++ index.js
@@ -0,0 +1,24 @@
+const Excel = require('xlsx');
+const fs = require('fs');
+require('dotenv').config();
+
+     fs.open( process.env.NODE_ROUTE_FS , (err , arq) => {
+     if (arq) {
+          const wb = Excel.readFile(arq, { dateNF: "dd/mm/yyyy"});
+               
+          const ws = wb.Sheets['log_carteira'];
+     
+          const data = Excel.utils.sheet_to_json(ws, {raw: false});
+               
+          fs.writeFile("./import_mysql.json", JSON.stringify(data), err =>{
+               if(err){
+                    console.log(err)
+               }
+               });
+                    
+          }else{
+               console.log(err)
+          }
+     });
+
+
